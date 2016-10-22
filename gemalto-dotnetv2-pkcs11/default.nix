439ce7c470646ce4c80f06a1193df842209be625
Ambroz Bizjak <ambrop7@gmail.com>
2016-10-22 18:41:08 +0200

    Update.

--- gemalto-dotnetv2-pkcs11/default.nix
+++ gemalto-dotnetv2-pkcs11/default.nix
@@ -2,11 +2,7 @@
 stdenv.mkDerivation rec {
   name = "gemalto-dotnetv2-pkcs11";
 
-  src = fetchsvn {
-    url = "https://svn.macosforge.org/repository/smartcardservices/trunk/SmartCardServices/src/PKCS11dotNetV2";
-    rev = 160;
-    sha256 = "1hv1qp12n6lymjh48g89lg7xkql8wdwhm197s3c8yakpskgq8bpr";
-  };
+  src = ./PKCS11dotNetV2.tar.bz2;
 
   buildInputs = [ pcsclite boost155 zlib automake autoconf libtool pkgconfig ];
 
